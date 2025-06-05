365c46f6438b28890da29ce777faf3b70ef17eb3
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-06-05 10:41:41 +0300

    added new topics

---
+++ input/area_of_circle.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+radius_of_circle = float(input("Enter a radius: "))
+area_of_circle = pi * radius_of_circle * radius_of_circle
+print("Area of the circle:",area_of_circle)
---
+++ random_number/example.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.random()) #standard random
+
+print(random.randrange(1,11)) # range random
+
+print(random.randint(1,5))
+
