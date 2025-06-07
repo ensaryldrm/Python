09a7893d5218d4e2c6fa9c9bbe0efa120032bae6
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-06-07 11:22:16 +0300

    added new topic

--- strings/string_format.py
+++ strings/string_format.py
@@ -2,3 +2,9 @@ age = 19
 name = "Ensar Yildirim"
 text = f"My name is {name}, I am {age} years old!"
 print(text)
+
+text2 = "The prince only {price} turkish liras!"
+print(text2.format(price = 70))
+
+text3 = "My name is {fullName}, I'm {age} years old!".format(fullName = "Ensar Yildirim", age = 19)
+print(text3)
