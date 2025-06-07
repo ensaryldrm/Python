66d0e6b600580b2fa0c7cc8e3eac84db91071340
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-06-07 11:32:53 +0300

    added if elif

---
+++ if_elseif/if_elseif.py
@@ -0,0 +1,6 @@
+if 70>7:
+    print("first number is greater than second number!")
+elif 70 == 7:
+    print("first number and second number are equal!")
+else:
+    print("first number is less than second number!")
