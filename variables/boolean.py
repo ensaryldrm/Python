25492731e8f3bb8e007b9f78b94760e40a9af078
Ensar Yıldırım <Ensar_Yldrm@outlook.com>
2025-06-07 11:32:59 +0300

    added boolean

---
+++ variables/boolean.py
@@ -0,0 +1,21 @@
+# TRUE OR FALSE
+print(7>4) # it is true!
+print(7 == 5) # it is false!
+
+print("----------------------------------")
+
+x = 19
+y = 70
+if x>y:
+    print(x,"is greater than",y)
+else:
+    print(x,"is less than",y)
+
+print("----------------------------------")
+
+a = bool("python")
+b = bool(7)
+print(a)
+print(b)
+print(type(a))
+print(type(b))
